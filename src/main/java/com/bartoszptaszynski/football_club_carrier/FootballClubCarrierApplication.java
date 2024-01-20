fd5faf8e19564a15184677c152a0a49a79e6520e
BartoszPtaszynski <bartoszptasz@gmail.com>
2024-01-20 21:36:45 +0100

    project inicializated

---
+++ src/main/java/com/bartoszptaszynski/football_club_carrier/FootballClubCarrierApplication.java
@@ -0,0 +1,13 @@
+package com.bartoszptaszynski.football_club_carrier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballClubCarrierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballClubCarrierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartoszptaszynski/football_club_carrier/FootballClubCarrierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartoszptaszynski.football_club_carrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballClubCarrierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
